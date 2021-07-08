33dab0767959f3399af465796db2e1e6226165d6
Matt You <matt.you@onedegree.hk>
2021-07-08 22:41:50 +0800

    format ms

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,13 +13,16 @@ const getTimeStamp = (dateString) => {
 
 const toTimeString = (s) => {
   const time = new Date(s * 1000);
-
   // return `${time.getFullYear()} / ${time.getMonth()} / ${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
   const format = new Intl.DateTimeFormat("zh-TW", {
     dateStyle: "full",
     timeStyle: "medium",
   });
-  return format.format(time) + "." + time.getMilliseconds();
+  const msString = time.getMilliseconds().toString();
+  const prefix = Array(3 - msString.length)
+    .fill("0")
+    .join();
+  return format.format(time) + "." + prefix + time.getMilliseconds();
 };
 
 const useCamera = ({ startTime, endTime, x, setCameraImgPath }) => {
